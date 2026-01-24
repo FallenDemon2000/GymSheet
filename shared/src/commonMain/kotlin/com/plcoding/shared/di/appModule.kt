23a1f845473280bd698b1c6f426e4b46a1742208
FallenDemon2000 <amendona6@gmail.com>
2026-01-24 20:07:13 +0000

    dev: add training day data source

--- shared/src/commonMain/kotlin/com/plcoding/shared/di/appModule.kt
+++ shared/src/commonMain/kotlin/com/plcoding/shared/di/appModule.kt
@@ -1,9 +1,14 @@
 package com.plcoding.shared.di
 
+import com.plcoding.shared.data.ds.api.TrainingDaysDataSource
+import com.plcoding.shared.data.ds.implementation.TrainingDaysDataSourceImpl
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val appModule = module {
     // Data Layer
+    singleOf(::TrainingDaysDataSourceImpl) bind TrainingDaysDataSource::class
 
     // Domain Layer
 
