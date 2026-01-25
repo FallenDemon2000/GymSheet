8d5289fccfe7ca4f03493803eaf3a66071c9bfa8
FallenDemon2000 <amendona6@gmail.com>
2026-01-25 15:13:25 +0000

    dev: add repository get exercises

--- shared/src/commonMain/kotlin/com/plcoding/shared/data/repository/implementation/TrainingDaysRepositoryImpl.kt
+++ shared/src/commonMain/kotlin/com/plcoding/shared/data/repository/implementation/TrainingDaysRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.plcoding.shared.data.repository.implementation
 
 import com.plcoding.shared.data.ds.api.TrainingDaysDataSource
 import com.plcoding.shared.data.repository.api.TrainingDaysRepository
+import com.plcoding.shared.presentation.model.Exercise
 import com.plcoding.shared.presentation.model.TrainingDay
 
 class TrainingDaysRepositoryImpl(
@@ -10,4 +11,7 @@ class TrainingDaysRepositoryImpl(
 
     override suspend fun getTrainingDays(): List<TrainingDay> =
         dataSource.getTrainingDays()
+
+    override suspend fun getExercises(day: Int): List<Exercise>? =
+        dataSource.getExercises(day)
 }
