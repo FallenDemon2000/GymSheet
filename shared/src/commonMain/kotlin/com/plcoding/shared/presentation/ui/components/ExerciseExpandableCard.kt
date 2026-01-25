fb96f4298eead2815f1adfeb82cde5e45028d5a8
FallenDemon2000 <amendona6@gmail.com>
2026-01-25 17:50:19 +0000

    dev: add exercises screen content view

---
+++ shared/src/commonMain/kotlin/com/plcoding/shared/presentation/ui/components/ExerciseExpandableCard.kt
@@ -0,0 +1,233 @@
+package com.plcoding.shared.presentation.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Remove
+import androidx.compose.material3.Card
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.plcoding.shared.ktx.formatNumber
+import com.plcoding.shared.presentation.model.Exercise
+import com.plcoding.shared.presentation.ui.theme.GymSheetTheme
+
+@Composable
+fun ExerciseExpandableCard(
+    exercise: Exercise,
+    isExpanded: Boolean,
+    onCardClick: () -> Unit,
+    onPlusClick: () -> Unit,
+    onMinusClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    var showEditDialog by remember { mutableStateOf(false) }
+    val onEditClick: () -> Unit = { showEditDialog = true }
+
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(4.dp),
+    ) {
+        IconButton(onClick = onMinusClick) {
+            Icon(
+                imageVector = Icons.Default.Remove,
+                contentDescription = "Edit icon",
+                tint = MaterialTheme.colorScheme.surfaceContainer,
+            )
+        }
+        ExerciseCard(
+            exercise = exercise,
+            onClick = onCardClick,
+            isExpanded = isExpanded,
+            modifier = Modifier.weight(1f),
+        )
+        Column {
+            IconButton(onClick = onPlusClick) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Edit icon",
+                    tint = MaterialTheme.colorScheme.surfaceContainer,
+                )
+            }
+            if (isExpanded) {
+                IconButton(onClick = onEditClick) {
+                    Icon(
+                        imageVector = Icons.Default.Edit,
+                        contentDescription = "Edit icon",
+                        tint = MaterialTheme.colorScheme.surfaceContainer,
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun ExerciseCard(
+    exercise: Exercise,
+    onClick: () -> Unit,
+    isExpanded: Boolean = false,
+    modifier: Modifier = Modifier,
+) {
+    Column(modifier = modifier) {
+        Card(modifier = Modifier.fillMaxWidth(), onClick = onClick) {
+            Row(
+                modifier = Modifier.fillMaxWidth().padding(12.dp),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Text(
+                    text = exercise.name,
+                    style = MaterialTheme.typography.titleMedium,
+                    overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier.weight(1f),
+                )
+                if (exercise.weight != null) {
+                    Text(
+                        text = "${formatNumber(exercise.weight)} Kg",
+                        style = MaterialTheme.typography.titleMedium,
+                    )
+                }
+            }
+        }
+        if (isExpanded) {
+            ExerciseDetails(
+                sets = exercise.sets,
+                repetitions = exercise.reps,
+                restingTime = exercise.restTime,
+                modifier = Modifier
+            )
+        }
+    }
+}
+
+@Composable
+private fun ExerciseDetails(
+    sets: Int,
+    repetitions: Int,
+    restingTime: Int,
+    modifier: Modifier = Modifier,
+) {
+    Row(modifier = modifier) {
+        ExerciseDescription(
+            sets = sets,
+            repetitions = repetitions,
+            restingTime = restingTime,
+            modifier = Modifier
+                .bottomBorder(MaterialTheme.colorScheme.surfaceContainer)
+                .padding(horizontal = 16.dp, vertical = 6.dp)
+                .weight(1f),
+        )
+    }
+}
+
+@Composable
+private fun ExerciseDescription(
+    sets: Int,
+    repetitions: Int,
+    restingTime: Int,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        ExerciseField(field = "Sets", value = sets)
+        ExerciseField(field = "Reps", value = repetitions)
+        HorizontalDivider(modifier = Modifier.fillMaxWidth())
+        ExerciseField(field = "Rest Time", value = restingTime)
+    }
+}
+
+@Composable
+private fun ExerciseField(
+    field: String,
+    value: Int,
+    modifier: Modifier = Modifier,
+) {
+    Row(modifier = modifier) {
+        Text(
+            text = field,
+            modifier = Modifier.weight(1f),
+        )
+        Text(
+            text = value.toString(),
+        )
+    }
+}
+
+private fun Modifier.bottomBorder(color: Color) =
+    drawBehind {
+        val strokeWidth = 2 * density
+        val y = size.height - strokeWidth / 2
+
+        drawLine(
+            color = color,
+            start = Offset(x = 0f, y = 0f),
+            end = Offset(x = 0f, y = y),
+            strokeWidth = strokeWidth
+        )
+
+        drawLine(
+            color = color,
+            start = Offset(x = 0f, y = y),
+            end = Offset(x = size.width, y = y),
+            strokeWidth = strokeWidth
+        )
+
+        drawLine(
+            color = color,
+            start = Offset(x = size.width, y = y),
+            end = Offset(x = size.width, y = 0f),
+            strokeWidth = strokeWidth
+        )
+    }
+
+@Preview
+@Composable
+private fun ExerciseExpandableCardPreview() {
+    GymSheetTheme {
+        ExerciseExpandableCard(
+            exercise = Exercise.empty,
+            isExpanded = false,
+            onCardClick = {},
+            onPlusClick = {},
+            onMinusClick = {},
+            modifier = Modifier.fillMaxWidth(),
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun ExerciseExpandableCardPreviewDarkTheme() {
+    GymSheetTheme(darkTheme = true) {
+        ExerciseExpandableCard(
+            exercise = Exercise.empty,
+            isExpanded = true,
+            onCardClick = {},
+            onPlusClick = {},
+            onMinusClick = {},
+            modifier = Modifier.fillMaxWidth(),
+        )
+    }
+}
--- shared/src/commonMain/kotlin/com/plcoding/shared/presentation/ui/screens/ExercisesScreen.kt
+++ shared/src/commonMain/kotlin/com/plcoding/shared/presentation/ui/screens/ExercisesScreen.kt
@@ -1,19 +1,91 @@
 package com.plcoding.shared.presentation.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.plcoding.shared.presentation.model.Exercise
+import com.plcoding.shared.presentation.ui.components.ErrorMessage
+import com.plcoding.shared.presentation.ui.components.ExerciseExpandableCard
 import com.plcoding.shared.presentation.ui.components.GymSheetAppBar
 import com.plcoding.shared.presentation.ui.components.LoadingIndicator
 import com.plcoding.shared.presentation.ui.theme.GymSheetTheme
+import com.plcoding.shared.presentation.ui.uistate.ExercisesUiState
+import com.plcoding.shared.presentation.viewmodel.ExercisesViewModel
+import org.koin.compose.koinInject
+import org.koin.core.parameter.parametersOf
 
 @Composable
 fun ExercisesScreen(
     trainingDay: Int,
     onBackClick: () -> Unit,
+    exercisesViewModel: ExercisesViewModel = koinInject { parametersOf(trainingDay) },
 ) {
+    val exercisesState by exercisesViewModel.uiState.collectAsState()
+
     Scaffold(topBar = getTopBar(trainingDay, onBackClick)) { paddingValues ->
-        LoadingIndicator()
+        when (exercisesState) {
+            is ExercisesUiState.Loading -> {
+                LoadingIndicator()
+            }
+
+            is ExercisesUiState.Error -> {
+                val errorState = exercisesState as ExercisesUiState.Error
+                ErrorMessage(message = errorState.message)
+            }
+
+            is ExercisesUiState.Success -> {
+                val state = exercisesState as ExercisesUiState.Success
+                ExercisesView(
+                    exercises = state.exercises,
+                    paddingValues = paddingValues,
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun ExercisesView(
+    exercises: List<Exercise>,
+    paddingValues: PaddingValues = PaddingValues(),
+) {
+    var expandedCardIndex by remember { mutableStateOf(-1) }
+
+    val onCardClick: (Int) -> Unit = {
+        expandedCardIndex =
+            if(expandedCardIndex == it) -1 else it
+    }
+
+    LazyColumn(
+        modifier = Modifier.fillMaxSize().padding(paddingValues),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+    ) {
+        itemsIndexed(items = exercises) { index, exercise ->
+            ExerciseExpandableCard(
+                exercise = exercise,
+                isExpanded = expandedCardIndex == index,
+                onCardClick = { onCardClick(index) },
+                onPlusClick = {},
+                onMinusClick = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 4.dp),
+            )
+        }
     }
 }
 
@@ -29,9 +101,8 @@ private fun getTopBar(day: Int, onBackClick: () -> Unit): @Composable (() -> Uni
 @Composable
 private fun ExercisesScreenPreview() {
     GymSheetTheme {
-        ExercisesScreen(
-            trainingDay = 0,
-            onBackClick = {},
+        ExercisesView(
+            exercises = List(7) { Exercise.empty },
         )
     }
 }
@@ -40,9 +111,8 @@ private fun ExercisesScreenPreview() {
 @Composable
 private fun ExercisesScreenPreviewDarkTheme() {
     GymSheetTheme(darkTheme = true) {
-        ExercisesScreen(
-            trainingDay = 0,
-            onBackClick = {},
+        ExercisesView(
+            exercises = List(7) { Exercise.empty },
         )
     }
 }
