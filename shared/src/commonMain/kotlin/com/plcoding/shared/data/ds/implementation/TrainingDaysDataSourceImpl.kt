8016aa576f2a4fe63d8f6d34f526c96580d03c20
FallenDemon2000 <amendona6@gmail.com>
2026-01-25 15:12:09 +0000

    dev: add data source get exercises

--- shared/src/commonMain/kotlin/com/plcoding/shared/data/ds/implementation/TrainingDaysDataSourceImpl.kt
+++ shared/src/commonMain/kotlin/com/plcoding/shared/data/ds/implementation/TrainingDaysDataSourceImpl.kt
@@ -2,6 +2,7 @@ package com.plcoding.shared.data.ds.implementation
 
 import com.plcoding.shared.data.ds.StaticData
 import com.plcoding.shared.data.ds.api.TrainingDaysDataSource
+import com.plcoding.shared.presentation.model.Exercise
 import com.plcoding.shared.presentation.model.TrainingDay
 import kotlinx.coroutines.delay
 
@@ -9,5 +10,10 @@ class TrainingDaysDataSourceImpl: TrainingDaysDataSource {
     override suspend fun getTrainingDays(): List<TrainingDay> {
         delay(2000L)
         return StaticData.staticTrainingDays
+    }
+
+    override suspend fun getExercises(day: Int): List<Exercise>? {
+        delay(1000L)
+        return StaticData.staticTrainingDays.firstOrNull { it.day == day }?.exercises
     }
 }
