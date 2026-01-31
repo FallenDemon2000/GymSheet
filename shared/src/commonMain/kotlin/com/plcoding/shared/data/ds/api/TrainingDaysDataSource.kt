583c1de877927090f0dc683978683406e1e31ba1
FallenDemon2000 <amendona6@gmail.com>
2026-01-31 22:54:41 +0000

    dev: remove unused training days data source

--- shared/src/commonMain/kotlin/com/plcoding/shared/data/ds/api/TrainingDaysDataSource.kt
+++
@@ -1,9 +0,0 @@
-package com.plcoding.shared.data.ds.api
-
-import com.plcoding.shared.presentation.model.Exercise
-import com.plcoding.shared.presentation.model.TrainingDay
-
-interface TrainingDaysDataSource {
-    suspend fun getTrainingDays(): List<TrainingDay>
-    suspend fun getExercises(day: Int): List<Exercise>?
-}
--- shared/src/commonMain/kotlin/com/plcoding/shared/di/AppModule.kt
+++ shared/src/commonMain/kotlin/com/plcoding/shared/di/AppModule.kt
@@ -3,8 +3,6 @@ package com.plcoding.shared.di
 import com.plcoding.shared.data.ds.ExercisesDao
 import com.plcoding.shared.data.ds.ExercisesDatabase
 import com.plcoding.shared.data.ds.ExercisesDbCallback
-import com.plcoding.shared.data.ds.api.TrainingDaysDataSource
-import com.plcoding.shared.data.ds.implementation.TrainingDaysDataSourceImpl
 import com.plcoding.shared.data.repository.api.TrainingDaysRepository
 import com.plcoding.shared.data.repository.implementation.TrainingDaysRepositoryImpl
 import com.plcoding.shared.presentation.viewmodel.ExercisesViewModel
@@ -19,7 +17,6 @@ val appModule = module {
     // Data Layer
     singleOf(::ExercisesDbCallback)
     single<ExercisesDao> { get<ExercisesDatabase>().exercisesDao() }
-    singleOf(::TrainingDaysDataSourceImpl) bind TrainingDaysDataSource::class
     singleOf(::TrainingDaysRepositoryImpl) bind TrainingDaysRepository::class
 
     // Domain Layer
